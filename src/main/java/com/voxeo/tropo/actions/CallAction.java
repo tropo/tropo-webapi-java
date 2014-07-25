6681276bc521d21002c609e026ca40878ed367a3
John Dyer <jdyer@tropo.com>
2014-07-25 10:28:37 -0400

    Work in progress

--- src/main/java/com/voxeo/tropo/actions/CallAction.java
+++ src/main/java/com/voxeo/tropo/actions/CallAction.java
@@ -7,32 +7,32 @@ import com.voxeo.tropo.Key;
 import com.voxeo.tropo.annotations.RequiredKeys;
 import com.voxeo.tropo.annotations.ValidKeys;
 
-@ValidKeys(keys={"to","allowSignals","answerOnMedia","channel","from","headers","name","network","recording","required","timeout"})
-@RequiredKeys(keys={"to"})
+@ValidKeys(keys = { "to", "allowSignals", "answerOnMedia", "channel", "from", "headers", "name", "network", "recording", "required", "timeout" })
+@RequiredKeys(keys = { "to" })
 public class CallAction extends JsonAction {
 
-	private ActionSupportHandler<StartRecordingAction> startRecordingActionSupportHandler = new ActionSupportHandler<StartRecordingAction>(StartRecordingAction.class);	
-	private HeadersSupportHandler headersSupportHandler = new HeadersSupportHandler();
-	
-	public CallAction() {
-		
-		super();
-		setName("call");
-	}
-	
-	public CallAction(Key... keys) {
-	
-		super(keys);
-		setName("call");
-	}	
-	
-	public StartRecordingAction startRecording(Key... keys) {
+  private ActionSupportHandler<StartRecordingAction> startRecordingActionSupportHandler = new ActionSupportHandler<StartRecordingAction>(StartRecordingAction.class);
+  private HeadersSupportHandler                      headersSupportHandler              = new HeadersSupportHandler();
 
-		return startRecordingActionSupportHandler.build(this, keys);
-	}
-	
-	public HeadersAction headers(String[]... keys) {
+  public CallAction() {
 
-		return headersSupportHandler.headers(this, keys);
-	}
+    super();
+    setName("call");
+  }
+
+  public CallAction(Key... keys) {
+
+    super(keys);
+    setName("call");
+  }
+
+  public StartRecordingAction startRecording(Key... keys) {
+
+    return startRecordingActionSupportHandler.build(this, keys);
+  }
+
+  public HeadersAction headers(String[]... keys) {
+
+    return headersSupportHandler.headers(this, keys);
+  }
 }
--- src/test/java/com/voxeo/tropo/AskActionTest.java
+++ src/test/java/com/voxeo/tropo/AskActionTest.java
@@ -20,38 +20,48 @@ public class AskActionTest {
 
 	@Test
 	public void testAsk() {
-		
+
 		Tropo tropo = new Tropo();
 		tropo
 			.ask(NAME("foo"),BARGEIN(true),TIMEOUT(30.0f),REQUIRED(true));
-		
+
 		assertEquals(tropo.text(),"{\"tropo\":[{\"ask\":{\"name\":\"foo\",\"bargein\":true,\"timeout\":30,\"required\":true}}]}");
 	}
-	
+
+	@Test
+	public void testAskWithSensitivity() {
+
+		Tropo tropo = new Tropo();
+		tropo
+			.ask(NAME("foo"),BARGEIN(true),TIMEOUT(30.0f),SENSITIVITY(30.0f),REQUIRED(true));
+
+		assertEquals(tropo.text(),"{\"tropo\":[{\"ask\":{\"name\":\"foo\",\"bargein\":true,\"sensitivity\":30.0,\"timeout\":30,\"required\":true}}]}");
+	}
+
 	@Test
 	public void testAskWithSayBlock() {
-		
+
 		Tropo tropo = new Tropo();
 		tropo
 			.ask(NAME("foo"),BARGEIN(true),TIMEOUT(30.0f),REQUIRED(true),CHOICES("[5 DIGITS]"))
 				.say("Please say your account number");
-		
+
 		assertEquals(tropo.text(),"{\"tropo\":[{\"ask\":{\"name\":\"foo\",\"bargein\":true,\"timeout\":30,\"required\":true,\"choices\":\"[5 DIGITS]\",\"say\":[{\"value\":\"Please say your account number\"}]}}]}");
 	}
-	
+
 	@Test
 	public void testAskWithSayAndOnBlocks() {
-		
+
 		Tropo tropo = new Tropo();
 		tropo
 			.ask(NAME("foo"),BARGEIN(true),TIMEOUT(30.0f),REQUIRED(true)).and(
-				Do.say("Please say your account number"), 
+				Do.say("Please say your account number"),
 				Do.on(EVENT("success"),NEXT("/result.json")),
 				Do.choices(VALUE("[5 DIGITS]")));
 
 		assertEquals(tropo.text(),"{\"tropo\":[{\"ask\":{\"name\":\"foo\",\"bargein\":true,\"timeout\":30,\"required\":true,\"say\":[{\"value\":\"Please say your account number\"}],\"on\":[{\"event\":\"success\",\"next\":\"/result.json\"}],\"choices\":{\"value\":\"[5 DIGITS]\"}}}]}");
 	}
-	
+
 	@Test
 	public void testFailsAskWithNoNameParameter() {
 
@@ -63,7 +73,7 @@ public class AskActionTest {
 			assertEquals(te.getMessage(), "Missing required property: 'name'");
 		}
 	}
-	
+
 	@Test
 	public void testAskFailsWithInvalidElement() {
 
@@ -75,62 +85,62 @@ public class AskActionTest {
 			assertEquals(te.getMessage(), "Invalid key 'to' for action");
 		}
 	}
-	
+
 	@Test
 	public void testAllowSignals() {
-		
+
 		Tropo tropo = new Tropo();
 		tropo.ask(NAME("foo"),BARGEIN(true),TIMEOUT(30.0f),REQUIRED(true),ALLOW_SIGNALS("exit","stopHold"));
-	
+
 		assertEquals(tropo.text(),"{\"tropo\":[{\"ask\":{\"name\":\"foo\",\"bargein\":true,\"timeout\":30,\"required\":true,\"allowSignals\":[\"exit\",\"stopHold\"]}}]}");
-	}	
-	
+	}
+
 	@Test
 	public void testAskAllParameters() {
-		
+
 		Tropo tropo = new Tropo();
 		tropo.ask(NAME("foo"),BARGEIN(true),TIMEOUT(30.0f),INTERDIGIT_TIMEOUT(5),REQUIRED(true),ALLOW_SIGNALS("exit","stopHold"),ATTEMPTS(5),MIN_CONFIDENCE(3),RECOGNIZER(Recognizer.SPANISH),VOICE(Voice.ALLISON));
-	
+
 		assertEquals(tropo.text(),"{\"tropo\":[{\"ask\":{\"name\":\"foo\",\"bargein\":true,\"timeout\":30,\"interdigitTimeout\":5,\"required\":true,\"allowSignals\":[\"exit\",\"stopHold\"],\"attempts\":5,\"minConfidence\":3,\"recognizer\":\"es-es\",\"voice\":\"allison\"}}]}");
 	}
 
 	@Test
 	public void testAskModeIsSupportedParameters() {
-		
+
 		Tropo tropo = new Tropo();
 		tropo.ask(NAME("foo"),MODE(DTMF),TIMEOUT(30.0f),REQUIRED(true),ALLOW_SIGNALS("exit","stopHold"),ATTEMPTS(5),MIN_CONFIDENCE(3),RECOGNIZER(Recognizer.SPANISH),VOICE(Voice.ALLISON));
-	
+
 		assertEquals(tropo.text(),"{\"tropo\":[{\"ask\":{\"name\":\"foo\",\"mode\":\"dtmf\",\"timeout\":30,\"required\":true,\"allowSignals\":[\"exit\",\"stopHold\"],\"attempts\":5,\"minConfidence\":3,\"recognizer\":\"es-es\",\"voice\":\"allison\"}}]}");
 	}
-	
+
 	@Test
 	public void testAskTraditionalWay() {
-		
+
 		Tropo tropo = new Tropo();
 		tropo.ask("foo",true,30.0f,true);
-		
+
 		assertEquals(tropo.text(),"{\"tropo\":[{\"ask\":{\"name\":\"foo\",\"bargein\":true,\"timeout\":30,\"required\":true}}]}");
 	}
-	
+
 	@Test
 	public void testAskAndChoicesTraditionalWay() {
-		
+
 		Tropo tropo = new Tropo();
 		tropo.ask("foo",true,30.0f,true).choices(VALUE("[5 DIGITS]"));
-		
+
 		assertEquals(tropo.text(),"{\"tropo\":[{\"ask\":{\"name\":\"foo\",\"bargein\":true,\"timeout\":30,\"required\":true,\"choices\":{\"value\":\"[5 DIGITS]\"}}}]}");
 	}
-	
+
 	@Test
 	public void testChoicesMethods() {
-		
+
 		String output = "{\"tropo\":[{\"ask\":{\"attempts\":3,\"name\":\"question\",\"bargein\":true,\"timeout\":20,\"required\":true,\"choices\":{\"value\":\"red,green,blue\"}}}]}";
 		// From evolution bug
-		Tropo tropo = new Tropo(); 
+		Tropo tropo = new Tropo();
 		tropo.ask(ATTEMPTS(3), NAME("question"), BARGEIN(true), TIMEOUT(20f), REQUIRED(true))
-			 .choices("red,green,blue"); 
+			 .choices("red,green,blue");
 		assertEquals(tropo.text(),output);
-		
+
 		tropo.reset();
 		tropo.ask(ATTEMPTS(3), NAME("question"), BARGEIN(true), TIMEOUT(20f), REQUIRED(true))
 		 	 .choices(VALUE("red,green,blue"));
