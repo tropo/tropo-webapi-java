ffe5a6ec403b8464586bc07bba819d0bda6d8967
Diego Wong <dwong@grubhub.com>
2014-11-24 11:18:28 -0500

    Added the wait action support handler

--- src/main/java/com/voxeo/tropo/Tropo.java
+++ src/main/java/com/voxeo/tropo/Tropo.java
@@ -1,49 +1,18 @@
 package com.voxeo.tropo;
 
-import static com.voxeo.tropo.Key.BARGEIN;
-import static com.voxeo.tropo.Key.BEEP;
-import static com.voxeo.tropo.Key.CHANNEL;
-import static com.voxeo.tropo.Key.EVENT;
-import static com.voxeo.tropo.Key.EXIT_TONE;
-import static com.voxeo.tropo.Key.FORMAT;
-import static com.voxeo.tropo.Key.FROM;
-import static com.voxeo.tropo.Key.NAME;
-import static com.voxeo.tropo.Key.NETWORK;
-import static com.voxeo.tropo.Key.NEXT;
-import static com.voxeo.tropo.Key.PASSWORD;
-import static com.voxeo.tropo.Key.SENSITIVITY;
-import static com.voxeo.tropo.Key.REQUIRED;
-import static com.voxeo.tropo.Key.SEND_TONES;
-import static com.voxeo.tropo.Key.TIMEOUT;
-import static com.voxeo.tropo.Key.TO;
-import static com.voxeo.tropo.Key.URL;
-import static com.voxeo.tropo.Key.USERNAME;
-import static com.voxeo.tropo.Key.VALUE;
+import com.voxeo.tropo.actions.*;
+import com.voxeo.tropo.enums.Channel;
+import com.voxeo.tropo.enums.Format;
+import com.voxeo.tropo.enums.Network;
+import support.ActionSupportHandler;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import support.ActionSupportHandler;
-
-import com.voxeo.tropo.actions.ArrayBackedJsonAction;
-import com.voxeo.tropo.actions.AskAction;
-import com.voxeo.tropo.actions.CallAction;
-import com.voxeo.tropo.actions.ChoicesAction;
-import com.voxeo.tropo.actions.ConferenceAction;
-import com.voxeo.tropo.actions.MessageAction;
-import com.voxeo.tropo.actions.OnAction;
-import com.voxeo.tropo.actions.RecordAction;
-import com.voxeo.tropo.actions.RedirectAction;
-import com.voxeo.tropo.actions.SayAction;
-import com.voxeo.tropo.actions.StartRecordingAction;
-import com.voxeo.tropo.actions.TransferAction;
-import com.voxeo.tropo.enums.Channel;
-import com.voxeo.tropo.enums.Format;
-import com.voxeo.tropo.enums.Network;
+import static com.voxeo.tropo.Key.*;
 
 public class Tropo extends ArrayBackedJsonAction {
 
@@ -61,6 +30,7 @@ public class Tropo extends ArrayBackedJsonAction {
   private ActionSupportHandler<TransferAction>       transferActionSupportHandler       = new ActionSupportHandler<TransferAction>(TransferAction.class);
   private ActionSupportHandler<CallAction>           callActionSupportHandler           = new ActionSupportHandler<CallAction>(CallAction.class);
   private ActionSupportHandler<MessageAction>        messageActionSupportHandler        = new ActionSupportHandler<MessageAction>(MessageAction.class);
+  private ActionSupportHandler<WaitAction>           waitActionSupportHandler        = new ActionSupportHandler<WaitAction>(WaitAction.class);
 
   public Tropo() {
 
@@ -236,6 +206,15 @@ public class Tropo extends ArrayBackedJsonAction {
   public MessageAction message(String to, Network network, String from, Channel channel) {
 
     return message(TO(to), NETWORK(network), FROM(from), CHANNEL(channel));
+  }
+
+  public WaitAction serverWait(Long  milliseconds) {
+
+      return serverWait(MILLISECONDS(milliseconds));
+  }
+
+  public WaitAction serverWait(Key... keys){
+      return waitActionSupportHandler.build(this, "tropo", keys);
   }
 
   public TropoResult parse(String json) {
