798c19003b48f8764d18fe246bde661c8c76c05e
pengxli <pengxli@cisco.com>
2017-03-21 16:11:12 +0800

    add some startRecording comments

--- src/main/java/com/voxeo/tropo/actions/StartRecordingAction.java
+++ src/main/java/com/voxeo/tropo/actions/StartRecordingAction.java
@@ -4,6 +4,17 @@ import com.voxeo.tropo.Key;
 import com.voxeo.tropo.annotations.RequiredKeys;
 import com.voxeo.tropo.annotations.ValidKeys;
 
+/**
+ * <p>
+ * Allows Tropo applications to begin recording the current session. The
+ * resulting recording may then be sent via FTP or an HTTP POST/Multipart Form.
+ * </p>
+ * <p>
+ * The audio file can also be transcribed and the text returned to you via an
+ * email address or HTTP POST/Multipart Form - currently, transcription only
+ * supports US English.
+ * </p>
+ */
 @ValidKeys(keys={"format","method","url","username","password","asyncUpload","transcriptionOutURI","transcriptionEmailFormat","transcriptionID"})
 @RequiredKeys(keys={"url"})
 public class StartRecordingAction extends JsonAction {
